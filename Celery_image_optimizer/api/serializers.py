c4b6b8fe0902f1e84b8d2ab107ce138f7f156324
TashifToor <tashiftoor12345@gmail.com>
2025-08-04 16:53:17 +0500

    hello

--- Celery_image_optimizer/api/serializers.py
+++
@@ -1,6 +0,0 @@
-from .models import Student
-from rest_framework import serializers
-class StudentSerialzier(serializers.ModelSerializer):
-    class Meta:
-        model=Student
-        fields="__all__"
--- Celery_image_optimizer/api/task.py
+++
@@ -1,7 +0,0 @@
-# from PIL import Image
-# from celery import shared_task
-
-# @shared_task
-# def optimize_image(path):
-#     img=Image.open(path)
-#     img.save(path,optimize=True,quality=70)
